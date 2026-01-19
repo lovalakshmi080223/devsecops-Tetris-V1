746fe4835a839c49d8c0b3bd8383b78b7e06a0d1
lovalakshmi080223 <lovalakshmianasuri@gmail.com>
2026-01-19 22:19:10 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tetris-argocd-bkt" # Replace with your actual S3 bucket name
+    bucket = "tetris-new-bkt" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
